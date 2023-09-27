aa799ac3800b6f27a05a84a5cc11bc5054a17ae4
Michael Yogar <michaelyogar878@gmail.com>
2023-09-27 10:14:45 -0400

    Add FieldArray to form

--- app/prep/page.tsx
+++ app/prep/page.tsx
@@ -1,47 +1,106 @@
 "use client";
-
 import React from "react";
-import { Formik, Form } from "formik";
-import { FormikInput } from "@/components/forms/FormikInput";
+import { Formik, Form, Field, FieldArray, ErrorMessage } from "formik";
 import * as Yup from "yup";
-import { useRouter, useSearchParams } from "next/navigation";
 
-const SignupForm = () => {
-  const router = useRouter();
-  const initialValues = {
-    question: "",
-    minutes: 0,
-    seconds: 0,
-  };
+const initialValues = {
+  question: "",
+  seconds: 0,
+  minutes: 0,
+};
 
-  const validationSchema = Yup.object().shape({
-    question: Yup.string().required(),
-    seconds: Yup.number().min(0).max(60).required(),
-    minutes: Yup.number().min(0).max(60).required(),
+const Page = () => {
+  const validationSchema = Yup.object({
+    forms: Yup.array()
+      .of(
+        Yup.object({
+          question: Yup.string().min(4, "too short").required("Required"),
+          seconds: Yup.number().min(0).max(60).required("Required"),
+          minutes: Yup.number().min(0).max(60).required("Required"),
+        })
+      )
+      .required("Must have friends")
+      .min(2, "Minimum of 2 question")
+      .max(5, "Maximum of 5 question"),
   });
 
-  const handleSubmit = (values, { setSubmitting }) => {
-    setTimeout(() => {
-      setSubmitting(false);
-      router.push("/video" + "?" + new URLSearchParams(values).toString());
-    }, 400);
-  };
   return (
-    <>
-      <Formik
-        initialValues={initialValues}
-        onSubmit={handleSubmit}
-        validationSchema={validationSchema}
-      >
+    <Formik
+      initialValues={{ forms: [initialValues] }}
+      validationSchema={validationSchema}
+      onSubmit={(values) =>
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+        }, 500)
+      }
+    >
+      {({ values, errors }) => (
         <Form>
-          <FormikInput label="Question" name="question" type="text" />
-          <FormikInput label="Minutes" name="minutes" type="number" />
-          <FormikInput label="Seconds" name="seconds" type="number" />
-          <button type="submit">Submit</button>
+          <FieldArray
+            name="forms"
+            render={(arrayHelpers) => (
+              <div>
+                {values.forms.map((formItem, index) => (
+                  <div key={index}>
+                    <div>
+                      <label htmlFor={`forms.${index}.question`}>
+                        Question
+                      </label>
+                      <Field
+                        id={`forms.${index}.question`}
+                        name={`forms.${index}.question`}
+                      />
+                      <ErrorMessage name={`forms.${index}.question`} />
+                    </div>
+
+                    <div>
+                      <label htmlFor={`forms.${index}.seconds`}>Seconds</label>
+                      <Field
+                        id={`forms.${index}.seconds`}
+                        name={`forms.${index}.seconds`}
+                      />
+                      <ErrorMessage name={`forms.${index}.seconds`} />
+                    </div>
+                    <div>
+                      <label htmlFor={`forms.${index}.minutes`}>Minutes</label>
+                      <Field
+                        id={`forms.${index}.minutes`}
+                        name={`forms.${index}.minutes`}
+                      />
+                      <ErrorMessage name={`forms.${index}.minutes`} />
+                    </div>
+
+                    {index > 0 && (
+                      <button
+                        type="button"
+                        onClick={() => arrayHelpers.remove(index)}
+                      >
+                        -
+                      </button>
+                    )}
+                    <button
+                      type="button"
+                      onClick={() =>
+                        arrayHelpers.insert(index + 1, initialValues)
+                      }
+                    >
+                      +
+                    </button>
+                  </div>
+                ))}
+                {typeof errors.forms === "string" ? (
+                  <div>{JSON.stringify(errors.forms)}</div>
+                ) : null}
+                <div>
+                  <button type="submit">Submit</button>
+                </div>
+              </div>
+            )}
+          />
         </Form>
-      </Formik>
-    </>
+      )}
+    </Formik>
   );
 };
 
-export default SignupForm;
+export default Page;
