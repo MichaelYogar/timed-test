c3eeee445db5364195fcfedc071ed223e3a74d15
Michael Yogar <michaelyogar878@gmail.com>
2023-10-02 11:30:50 -0400

    Placeholder result page

--- app/(pages)/result/page.tsx
+++ app/(pages)/result/page.tsx
@@ -1,34 +1,5 @@
 "use client";
 
-import { VIDEO_KEY, getVideos } from "@/lib/idb";
-import { Prisma } from "@prisma/client";
-import useSWR from "swr";
-
-const fetcher = async (): Promise<any[]> => await getVideos();
-
-const Page: React.FC<Prisma.QuestionCreateInput> = () => {
-  const { data, error, isLoading } = useSWR(VIDEO_KEY, fetcher);
-
-  if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>{error.toString()}</p>;
-
-  return (
-    <>
-      <div>
-        {data &&
-          data.map((blob, index) => {
-            if (blob)
-              return (
-                <video
-                  key={index}
-                  src={URL.createObjectURL(blob.video)}
-                  controls
-                />
-              );
-          })}
-      </div>
-    </>
-  );
-};
+const Page = () => <div>Result page</div>;
 
 export default Page;
