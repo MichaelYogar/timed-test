c72d7079f99dd547c590c39cffae41c88018a2d8
Michael Yogar <michaelyogar878@gmail.com>
2023-10-07 15:06:56 -0400

    Add landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 import Link from "next/link";
-export default function Home() {
+
+const page = () => {
   return (
-    <div>
-      <Link href={"/create"}>Try for free</Link>
-    </div>
+    <article className="container mx-auto prose prose-sm md:prose pt-8">
+      <h3>About</h3>
+      <div className="flex justify-end">
+        <Button variant="link" asChild>
+          <Link href="/create">Try me</Link>
+        </Button>
+      </div>
+      <div className="mb-8">Currently at prototype stage.</div>
+      <blockquote className="mt-6 border-l-2 pl-6 italic">
+        When to use iterative development? You should use iterative development
+        only on projects that you want to succeed.
+        <p>Martin Fowler</p>
+      </blockquote>
+      <Separator />
+      <div className="flex justify-center my-2">
+        <a>Github</a>
+      </div>
+    </article>
   );
-}
+};
+
+export default page;
