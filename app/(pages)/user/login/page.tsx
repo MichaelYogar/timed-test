a4acec558ebef42497be334ac5d98fd423cb9b44
michaelyogar <michaelyogar878@gmail.com>
2023-10-13 15:55:22 -0400

    Add error message for invalid login

--- app/(pages)/user/login/page.tsx
+++ app/(pages)/user/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { signIn } from "next-auth/react";
+import { SignInResponse, signIn } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { SubmitHandler, useForm } from "react-hook-form";
@@ -16,25 +16,34 @@ const Page = () => {
     register,
     handleSubmit,
     formState: { errors },
+    setError,
   } = useForm<LoginInput>();
 
   const router = useRouter();
 
   const onSubmit: SubmitHandler<LoginInput> = async (data) => {
-    const result = await signIn("credentials", {
+    const result = (await signIn("credentials", {
       redirect: false,
       username: data.username,
       password: data.password,
-    });
+    })) as SignInResponse;
+
+    if (result.status === 401) {
+      console.log(errors);
+        setError("password", {"type": "custom", "message": "Wrong password"});
+      return;
+    }
+
     const url = new URL(result?.url as string);
     const redirectUrl = url.searchParams.get("callbackUrl")!;
-    if (result?.ok) {
+    if (result.ok) {
       if (redirectUrl) router.push(redirectUrl);
       else {
         router.refresh();
         router.back();
       }
     }
+
   };
 
   return (
@@ -46,7 +55,7 @@ const Page = () => {
 
         <label>Password</label>
         <input {...register("password", { required: true })} />
-        {errors.password && <span>This field is required</span>}
+        {errors.password && <span>{errors.password.message}</span>}
 
         <Button variant="outline">Submit</Button>
         <div>
