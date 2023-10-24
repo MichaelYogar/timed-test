466a5c9551aa0bbfd830301302883a65f886b741
michaelyogar <michaelyogar878@gmail.com>
2023-10-23 20:28:13 -0400

    Removed SVG

--- src/components/SelectInterviewForm.tsx
+++ src/components/SelectInterviewForm.tsx
@@ -101,24 +101,7 @@ export const SelectInterviewForm: React.FC<InterviewFormProps> = ({
             );
           })}
           <div className="">
-            <Button>
-              Start
-              <svg
-                className="w-3.5 h-3.5 ml-2"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
-                />
-              </svg>
-            </Button>
+            <Button>Start</Button>
             <div>{errors.id?.message}</div>
           </div>
           <div className="inline-block">
