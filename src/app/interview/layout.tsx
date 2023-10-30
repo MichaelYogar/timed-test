b9ac939613e28228d75e525e63cfc380162c050d
Michael Yogar <michaelyogar878@gmail.com>
2023-10-30 14:06:47 -0400

    Navigate to different pages with layout

--- src/app/interview/layout.tsx
+++ src/app/interview/layout.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import { NavBar } from "@/src/components/ui/Navbar";
-import { PlusIcon } from "@radix-ui/react-icons";
-import { Text } from "@radix-ui/themes";
+import { Button, Text } from "@radix-ui/themes";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { CiSquarePlus, CiEdit } from "react-icons/ci";
+import { VscBook } from "react-icons/vsc";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const session = useSession();
+  const router = useRouter();
 
   return (
     <div className="w-screen h-screen">
@@ -14,16 +17,31 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
         <div className="grid-in-header bg-blue-100">
           <NavBar username={session.data?.user.name} />
         </div>
-        <div className="grid-in-sidebar justify-self-center">
-          <div className="flex flex-row items-center gap-2 my-2">
-            <PlusIcon /> <Text size="5">Your tests</Text>
-          </div>
-          <div className="flex flex-row items-center gap-2 my-2">
-            <PlusIcon /> <Text size="5">Edit tests</Text>
-          </div>
-          <div className="flex flex-row items-center gap-2 my-2">
-            <PlusIcon /> <Text size="5">Create tests</Text>
-          </div>
+        <div className="grid-in-sidebar justify-self-center w-full">
+          <Button
+            color="green"
+            variant="soft"
+            onClick={() => router.push("/interview/edit")}
+            className="flex flex-row items-center gap-2 my-2 cursor-pointer w-full"
+          >
+            <CiEdit size={30} /> <Text size="5">Edit</Text>
+          </Button>
+          <Button
+            color="indigo"
+            variant="soft"
+            onClick={() => router.push("/interview/create")}
+            className="flex flex-row items-center gap-2 my-2 cursor-pointer w-full"
+          >
+            <CiSquarePlus size={30} /> <Text size="5">Create</Text>
+          </Button>
+          <Button
+            color="red"
+            variant="soft"
+            onClick={() => router.push("/interview/select")}
+            className="flex flex-row items-center gap-2 my-2 cursor-pointer w-full"
+          >
+            <VscBook size={30} /> <Text size="5">Start</Text>
+          </Button>
         </div>
         <div className="grid-in-main bg-red-100">{children}</div>
       </div>
